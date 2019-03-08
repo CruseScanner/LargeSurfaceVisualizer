f1123db263da3d8262370ba1b9fca83ee5d735d0
Manuel Massing <m.massing@virtualities.de>
2019-03-08 16:47:56 +0100

    Implement custom shader & normal mapping.

---
+++ examples/cruse-webviewer/scanviewer.frag.glsl
@@ -0,0 +1,1 @@
+../../sources/cruse/scanviewer.frag.glsl
---
+++ sources/cruse/scanviewer.vert.glsl
@@ -0,0 +1,37 @@
+#ifdef GL_FRAGMENT_PRECISION_HIGH
+    precision highp float;
+#else
+    precision mediump float;
+#endif
+
+#define SHADER_NAME ScanViewerVertex
+
+attribute vec3 Vertex;
+attribute vec2 TexCoord0;
+
+uniform mat3 uModelViewNormalMatrix;
+uniform mat4 uModelViewMatrix;
+uniform mat4 uProjectionMatrix;
+varying vec2 vTexCoord0;
+
+#ifndef WITH_NORMAL_MAP
+varying vec3 vViewNormal;
+#endif
+
+varying vec4 vVertexColor;
+varying vec4 vViewVertex;
+
+void main() 
+{
+    vec4 viewVertex = uModelViewMatrix*vec4(Vertex.xyz, 1.0);
+    gl_Position = uProjectionMatrix*viewVertex;
+
+#ifndef WITH_NORMAL_MAP
+    vec3 viewNormal;
+    viewNormal = uModelViewNormalMatrix*vec3(0.0, 0.0, 1.0);
+    vViewNormal = viewNormal;
+#endif
+
+    vViewVertex = viewVertex;
+    vTexCoord0 = TexCoord0;
+}
