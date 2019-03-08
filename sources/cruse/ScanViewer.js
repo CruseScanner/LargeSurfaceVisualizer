656eb54785a709bed2a9e8d4760e2e574bc62fce
Manuel Massing <m.massing@virtualities.de>
2019-03-08 14:09:37 +0100

    Initialization cleanups; refactoring/preparations for normal map rendering.

--- sources/cruse/ScanViewer.js
+++ sources/cruse/ScanViewer.js
@@ -1,15 +1,35 @@
 import osg from 'osg/osg';
 import osgDB from 'osgDB/osgDB';
 import osgViewer from 'osgViewer/osgViewer';
+import defined from 'cruse/defined';
 
 'use strict';
+
+function initializeRootNode(scanViewer) {
+    return scanViewer.createTileForGeometry(
+        0,
+        0,
+        0
+    ).then(function(rootTile) {
+        scanViewer.viewer.setSceneData(rootTile);
+        var boundingSphere = rootTile.getBound();
+        scanViewer.viewer.setupManipulator();
+        scanViewer.viewer.getManipulator().setMinSpeed(256*10);
+        scanViewer.viewer.getManipulator().setDistance(boundingSphere.radius() * 1.5);
+    });
+};
+
 
 /**
  * @class ScanViewer
  * @constructor
  * 
  */
-var ScanViewer = function(canvasElement) {
+var ScanViewer = function(canvasElement, textureMapTileSource, normalMapTileSource) {
+    this._renderTextureMaps = false;
+    this._renderNormalMaps = false;
+    
+
     this._input = new osgDB.Input();
     this.projectedTilePixels = Math.PI/4.0*256*256;
     
@@ -17,10 +37,59 @@ var ScanViewer = function(canvasElement) {
         enableFrustumCulling: true
     });
     this.viewer.init();
+    
+    var tileServicePromises = [];    
+    var that = this;
+    if (defined(textureMapTileSource)) {
+        this._textureMapTileSource = textureMapTileSource;
+        tileServicePromises.push(textureMapTileSource.initializationPromise.then(function() {
+            that._renderTextureMaps = true;
+        }));
+    }
+    if (defined(normalMapTileSource)) {
+        this._normalMapTileSource = normalMapTileSource;
+        tileServicePromises.push(normalMapTileSource.initializationPromise.then(function(){
+            that._renderNormalMaps = true;
+        }));
+    }
    
-};
+    this._initializationPromise = Promise.all(tileServicePromises).then(function() {
+        initializeRootNode(that);
+    });
+      
+};  
 
-ScanViewer.prototype = {      
+ScanViewer.prototype = {        
+    fetchAndApplyTileImagery: function(x, y, level, stateSet, textureIndex, tileSource) {       
+        var image = new osg.Image();
+        var options = {
+                imageCrossOrigin : true            
+        };
+        
+        var url = tileSource.getTileURL(x, y, level);       
+        return this._input.fetchImage(image, url, options).then(function(img) {
+            var texture = new osg.Texture();
+            texture.setImage(img);
+            return texture;
+        }).then(function(texture) {
+            stateSet.setTextureAttributeAndModes(textureIndex, texture);
+        });        
+    },
+        
+    fetchAndApplyAllTileImagery: function(x, y, level, stateSet) {
+        var promises = [];
+        if (this._renderTextureMaps)
+        {
+            promises.push(this.fetchAndApplyTileImagery(x, y, level, stateSet, 0, this._textureMapTileSource));
+        }
+        
+        if (this._renderNormalMaps)
+        {
+            promises.push(this.fetchAndApplyTileImagery(x, y, level, stateSet, 1, this._normalMapTileSource));
+        }
+        return Promise.all(promises);
+    },
+        
     /**
      * Fetches normal and texture maps for the given tile, and returns a promise
      * to an osg geometry rendering the tile with textures and normals.
@@ -29,14 +98,13 @@ ScanViewer.prototype = {
      * 
      */
     createTileForGeometry: function(x, y, level) {
-        var tileGeometry = this._tileSource.getTileExtent(x, y, level);
+        // TODO: implement getTileExtent which dispatches between tmap/normal map as required
+        var tileGeometry = this._textureMapTileSource.getTileExtent(x, y, level);
         var x0 = tileGeometry.x0;
         var y0 = tileGeometry.y0;
         var width = (tileGeometry.x1-tileGeometry.x0)+1;
         var height = (tileGeometry.y1-tileGeometry.y0)+1;
         
-        var url = this._tileSource.getTileURL(x, y, level);
-
         var that = this;
         var createPagedLODGroup = function(parent) {
             var childPromises = [];
@@ -44,7 +112,7 @@ ScanViewer.prototype = {
             for (var i = 0; i < 4; i++) {
                 var addr = that.childAddress(i, parent.x, parent.y, parent.level);
                 (function(child) {
-                    if (that._tileSource.hasTile(child.x, child.y, child.level)) {
+                    if (that._textureMapTileSource.hasTile(child.x, child.y, child.level)) {
                         childPromises.push(that.createTileForGeometry(
                             child.x,
                             child.y,
@@ -63,33 +131,21 @@ ScanViewer.prototype = {
                 }
                 return group;
             });                
-        }
-        
-        var image = new osg.Image();
-        var options = {
-                imageCrossOrigin : true            
         };
-
-        return this._input.fetchImage(image, url, options).then(function(img) {
-            var texture = new osg.Texture();
-            texture.setImage(img);
-            return texture;
-        }).then(function(texture) {
-            var tileGeometry = osg.createTexturedQuadGeometry(x0, y0, 0, width, 0, 0, 0, height, 0, 0, 1, 1, 0);
-            var material = new osg.Material();
-            material.setDiffuse ([1.0, 1.0, 1.0, 1.0]);           
-            material.setAmbient ([0.3, 0.3, 0.3, 1.0]);
-            material.setEmission([0.0, 0.0, 0.0, 1.0]);
-            material.setSpecular([0.0, 0.0, 0.0, 1.0]);
-            material.setShininess(1.0);
-            
-            var stateSet = tileGeometry.getOrCreateStateSet(); 
-            stateSet.setAttributeAndModes(material);
-            stateSet.setTextureAttributeAndModes(0, texture);
-            
-            
+        
+        var tileGeometry = osg.createTexturedQuadGeometry(x0, y0, 0, width, 0, 0, 0, height, 0, 0, 1, 1, 0);
+        var material = new osg.Material();
+        material.setDiffuse ([1.0, 1.0, 1.0, 1.0]);           
+        material.setAmbient ([0.3, 0.3, 0.3, 1.0]);
+        material.setEmission([0.0, 0.0, 0.0, 1.0]);
+        material.setSpecular([0.0, 0.0, 0.0, 1.0]);
+        material.setShininess(1.0);
+        var stateSet = tileGeometry.getOrCreateStateSet(); 
+        stateSet.setAttributeAndModes(material);
+     
+        return this.fetchAndApplyAllTileImagery(x, y, level, stateSet).then(function() {
             var tile;          
-            if (that._tileSource.hasChildren(x, y, level)) {
+            if (that._textureMapTileSource.hasChildren(x, y, level)) {
                 // LOD node
                 tile = new osg.PagedLOD();
                 tile.setRangeMode(osg.PagedLOD.PIXEL_SIZE_ON_SCREEN);
@@ -125,23 +181,12 @@ ScanViewer.prototype = {
         };
     },
    
-    setupRootNode() {
-        var that = this;
-        return that.createTileForGeometry(
-            0,
-            0,
-            0
-        ).then(function(rootTile) {
-            that.viewer.setSceneData(rootTile);
-            var boundingSphere = rootTile.getBound();
-            that.viewer.setupManipulator();
-            that.viewer.getManipulator().setMinSpeed(256*10);
-            that.viewer.getManipulator().setDistance(boundingSphere.radius() * 1.5);
-        });
-    },
-    
+   
     run: function() {
-        this.viewer.run();
+        var that = this;
+        this._initializationPromise.then(function() {
+            that.viewer.run();
+        });
     }
 };
 
