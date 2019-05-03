f92bbe749cfa5b3931b8ab2ac1f7e6c95c0963c9
Patrick Degener <dd@crusescanner.com>
2019-05-03 12:29:21 +0200

    prepared 2d viewer for gallery

--- sources/cruse2DViewer/cruse-viewer.js
+++ sources/cruse2DViewer/cruse-viewer.js
@@ -3,9 +3,6 @@ import 'external/openseadragon-scalebar';
 
 var Cruse2DViewer = function(element, imageSource){
   
-  var prefix = imageSource.prefix || '?IIIF='
-  var fullUrl = imageSource.server + prefix + imageSource.image + '/info.json';  
-
   if(typeof(element) == 'string')
   {
     element = document.getElementById(element);
@@ -18,34 +15,18 @@ var Cruse2DViewer = function(element, imageSource){
 
   this.viewer = OpenSeadragon({
     element: element,
-    tileSources:   [fullUrl],
     toolbar: 'cruse-scanviewer-2d-toolbar',
   });
 
-  if(imageSource.scale != undefined)
-  {
-    this.viewer.scalebar({
-      type: 2,
-      stayInsideImage: false,
-      pixelsPerMeter: imageSource.scale * 1000,
-      backgroundColor: "rgba(0,0,0,0.5)",
-      fontColor: "rgba(255,255,255,0.5)",
-      color: "rgba(255,255,255,0.5)", 
-    });
-  }
-  
   // small hack to fix position: the viewer seems to switch the positon 
-  // to relative. Also move it to the end of the elements to fix draw order
+  // to relative. 
   toolbarElement.style.position = "absolute";
 
-  var captionElement = document.createElement('div');
-  captionElement.className = 'cruse-scanviewer-caption';
-  element.appendChild(captionElement);
+  this.captionElement = document.createElement('div');
+  this.captionElement.className = 'cruse-scanviewer-caption';
+  element.appendChild(this.captionElement);
 
-  if(imageSource.caption != undefined)
-  {
-    captionElement.innerHTML = imageSource.caption;
-  } 
+  this.open(imageSource);
 };
 
 Cruse2DViewer.prototype = {
@@ -57,6 +38,47 @@ Cruse2DViewer.prototype = {
   restoreView : function(view)
   {
     this.viewer.viewport.fitBounds(view);
+  },
+
+  open: function(imageSource)
+  {
+    if(imageSource == undefined)
+    {
+      return;
+    }
+
+    var prefix = imageSource.prefix || '?IIIF='
+    var fullUrl = imageSource.server + prefix + imageSource.image + '/info.json';  
+ 
+    this.viewer.open(fullUrl);
+  
+    if(imageSource.scale != undefined)
+    {
+      this.viewer.scalebar({
+        type: 2,
+        stayInsideImage: false,
+        pixelsPerMeter: imageSource.scale * 1000,
+        backgroundColor: "rgba(0,0,0,0.5)",
+        fontColor: "rgba(255,255,255,0.5)",
+        color: "rgba(255,255,255,0.5)", 
+      });
+    }
+    else
+    {
+      this.viewer.scalebar(
+        {
+          type: 0,
+        });      
+    }
+    
+    if(imageSource.caption != undefined)
+    {
+      this.captionElement.style.visibility = "visible";
+      this.captionElement.innerHTML = imageSource.caption;
+    } 
+    else{
+      this.captionElement.style.visibility = "hidden";
+    }
   }
 };
 
