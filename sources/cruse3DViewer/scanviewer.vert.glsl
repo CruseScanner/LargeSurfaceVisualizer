f7fe271c8620090f5689818ededb153ff4456ed7
Manuel Massing <m.massing@virtualities.de>
2019-05-07 11:42:10 +0200

    Support mapping of parents' textures for tiles which have elevation data, but no texture or normal map data (this occurs due to subsampling of elevation data).

--- sources/cruse3DViewer/scanviewer.vert.glsl
+++ sources/cruse3DViewer/scanviewer.vert.glsl
@@ -15,6 +15,8 @@ uniform mat4 uModelViewMatrix;
 uniform mat4 uProjectionMatrix;
 varying vec2 vTexCoord0;
 
+uniform vec4 uDiffuseMapOffsetScale;
+
 #ifndef WITH_NORMAL_MAP
 varying vec3 vViewNormal;
 #endif
@@ -41,6 +43,9 @@ void main()
 #endif
     // Clip vertex to [0..1] domain   
     vertex = min(vec3(1.0), max(vec3(0.0), vertex));
+
+    // Derive texture coordinates
+    vTexCoord0 = vertex.xy*uDiffuseMapOffsetScale.zw + uDiffuseMapOffsetScale.xy;
   
     vertex = vec3(vertex.xy*uOffsetScale.zw + uOffsetScale.xy, 0.0);
     
@@ -51,11 +56,9 @@ void main()
         
     vec4 viewVertex = uModelViewMatrix*vec4(vertex, 1.0);
     gl_Position = uProjectionMatrix*viewVertex;
-
+    vViewVertex = viewVertex;
+    
 #ifndef WITH_NORMAL_MAP
     vViewNormal = uModelViewNormalMatrix*vec3(0.0, 0.0, 1.0);
 #endif
-
-    vViewVertex = viewVertex;
-    vTexCoord0 = Vertex; //TexCoord0;
 }
