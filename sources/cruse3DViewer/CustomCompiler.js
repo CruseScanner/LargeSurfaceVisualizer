18f34bd26bcf2d478d28b09de5724d8bdd996d94
Patrick Degener <dd@crusescanner.com>
2019-12-19 16:08:36 +0100

    First draft of custom compiler

---
+++ sources/cruse3DViewer/CustomCompiler.js
@@ -0,0 +1,67 @@
+import OSG from 'external/osg';
+
+'use strict';
+
+var osgShader = OSG.osgShader;
+var osg = OSG.osg;
+
+// this compiler use basic lighting and add a node to demonstrate how to
+// customize the shader compiler
+var CustomCompiler = function() {
+    osgShader.Compiler.apply(this, arguments);
+};
+
+// we use same attributes than the default compiler
+var config = osgShader.Compiler.cloneStateAttributeConfig(osgShader.Compiler);
+config.attribute.push('Displacement');
+osgShader.Compiler.setStateAttributeConfig(CustomCompiler, config);
+
+CustomCompiler.prototype = osg.objectInherit(osgShader.Compiler.prototype, {
+  
+    getCompilerName: function() {
+        return 'DisplacementCompiler';
+    },
+
+    getOrCreateLocalVertex: function() {
+        var localVertex = osgShader.Compiler.prototype.getLighting.getOrCreateLocalVertex(this);
+
+        // ======================================================
+        // custom attribute displacement
+        // it's here I connect ouput of light result with my ramp
+        // ======================================================
+        var displacementAttribute = this.getAttributeType('Displacement');
+
+        if (displacementAttribute && displacementAttribute.getAttributeEnable()) {
+            var displacementResult = this.createVariable('vec3');
+
+            var uEnableDisplacement = displacementAttribute.getOrCreateUniforms().enableDisplacement;
+            var uDiffuseMapOffsetScale = displacementAttribute.getOrCreateUniforms().diffuseMapOffsetScale;
+            var uDisplacementOffsetScale = displacementAttribute.getOrCreateUniforms().displacementOffsetScale;
+            var uOffsetScale = displacementAttribute.getOrCreateUniforms().offsetScale;
+            var uDisplacementRange = displacementAttribute.getOrCreateUniforms().displacementRange;
+            
+            
+            this.getNode('DisplaceVertex')
+                .inputs({
+                    enableDisplacement: this.getOrCreateUniform(uEnableDisplacement),
+                    Vertex: this.getOrCreateAttribute('vec2', 'Vertex'),
+                    offsetScale: this.getOrCreateUniform(uOffsetScale),
+                    diffuseMapOffsetScale: this.getOrCreateUniform(uDiffuseMapOffsetScale),
+                    displacementOffsetScale: this.getOrCreateUniform(uDisplacementOffsetScale),
+                    displacementRange: this.getOrCreateUniform(uDisplacementRange),
+                    displacementMap,              
+                })
+                .outputs({
+                    vertexOutput: displacementResult
+                });
+
+            localVertex = displacementResult;
+        }
+
+        return localVertex;
+    },
+
+});
+
+export default CustomCompiler;
+
---
+++ sources/cruse3DViewer/displacement.glsl
@@ -0,0 +1,36 @@
+#ifdef GL_FRAGMENT_PRECISION_HIGH
+    precision highp float;
+#else
+    precision mediump float;
+#endif
+
+#pragma DECLARE_FUNCTION
+void DisplaceVertex( const in int enableDisplacement, 
+                     const in vec2 Vertex, 
+                     const in vec4 diffuseMapOffsetScale, 
+                     const in vec4 displacementOffsetScale, 
+                     const in float displacementRange,
+                     const in sampler2D displacementMap,
+                     out vec3 vertexOutput ) 
+{
+    // We encode skirts by placing them outside the [0..1]^2 domain
+    // We then reproject onto the boundary (by clamping), and use the distance 
+    // between original and clamped vertex as skirt displacement    
+    vec2 d = max(-Vertex, Vertex - vec2(1.0));
+    float displace = max(0.0, max(d.x, d.y));
+
+    vec3 vertex = vec3(Vertex, 0.0);
+
+     // Clip vertex to [0..1] domain   
+    vertex = min(vec3(1.0), max(vec3(0.0), vertex));
+
+    // Derive texture coordinates
+    vTexCoord0 = vertex.xy*diffuseMapOffsetScale.zw + diffuseMapOffsetScale.xy; 
+    vertexOutput = vec3(vertex.xy*uOffsetScale.zw + uOffsetScale.xy, 0.0);
+    
+    if( enableDisplacement == 1)
+    {
+        vec2 displacementUV = Vertex*displacementOffsetScale.zw + displacementOffsetScale.xy;  
+        vertex.z+= (texture2D(displacementMap, displacementUV).r-displace)*displacementRange;
+    } 
+}
